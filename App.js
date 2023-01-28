eb269461df70fc16397e6920733e01cdb6746f27
Noam Norman <normannoam21@gmail.com>
2023-01-28 19:05:01 +0200

    login - update

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LogIn from './Component/LogIn';
+import Welcome from './Component/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LogIn />
+      <LogIn />      
+      <StatusBar style="auto" />
     </View>
   );
 }
