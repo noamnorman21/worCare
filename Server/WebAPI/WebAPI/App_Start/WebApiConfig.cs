0f06688d7be720308fa0d4275ac2472b7e14ed15
Noam Norman <normannoam21@gmail.com>
2023-01-28 23:01:49 +0200

    server

--- Server/WebAPI/WebAPI/App_Start/WebApiConfig.cs
+++ Server/WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+               
             // Web API routes
             config.MapHttpAttributeRoutes();
 
