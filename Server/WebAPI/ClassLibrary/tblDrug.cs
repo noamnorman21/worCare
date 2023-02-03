a6307eeb76192204306b142084de4c3c0ede5844
Noam Norman <normannoam21@gmail.com>
2023-02-03 14:16:19 +0200

    Server

--- Server/WebAPI/ClassLibrary/tblDrug.cs
+++ Server/WebAPI/ClassLibrary/tblDrug.cs
@@ -24,6 +24,7 @@ namespace ClassLibrary
         public string drugName { get; set; }
         public string drugUrl { get; set; }
         public Nullable<System.DateTime> modifyDate { get; set; }
+        public string Type { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tblDrugForPatient> tblDrugForPatient { get; set; }
