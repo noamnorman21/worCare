e7bce064cf8e9aecf0ec5f40f16026438d60b288
oryan1410 <oryan1410@gmail.com>
2023-02-09 12:37:56 +0200

    paychecks and payment navigation base

--- Client/Component/Paychecks.jsx
+++ Client/Component/Paychecks.jsx
@@ -1,23 +1,71 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import CustomHeader from './AppBarUp';
 
-export default function PayChecks() {
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+
+
+export default function Paychecks() {
+  return ( 
+      <Tab.Navigator initialRouteName="Pending" style={{paddingTop:30}} >
+      <Tab.Screen name="Pending" component={Pending} />
+      <Tab.Screen name="History" component={History} />
+      
+    </Tab.Navigator>
+  );
+}
+
+function Main() {
   return (
-    <View style={styles.container}>
-      <Text>PayChecks</Text>
+    <View>
+      <Text>Main</Text>
     </View>
-  )
+  );
+}
+
+function Pending() {
+  return (
+    <View style={styles.container} >
+      <Text>Pending</Text>
+    </View>
+  );
+}
+
+function History() {
+  return (
+    <View style={styles.container} >
+      <Text>History</Text>
+    </View>
+  );
 }
 
 
-const styles = StyleSheet.create({
+
+const styles= StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30,
-    flexDirection: 'column',
-    fontFamily: 'sans-serif',
-    backgroundColor: 'none',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    
+    
+    },
+    container2: {
+      flex: 1,
+      paddingTop: 30,
+      flexDirection: 'column',
+      fontFamily:'sans-serif',
+      backgroundColor: 'none',
+fontWeight: 'bold',      }
+
 })
+     
+
+
+
+
+
+
+ 
--- Client/Component/PaymentsInner.jsx
+++ Client/Component/PaymentsInner.jsx
@@ -1,23 +1,65 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import CustomHeader from './AppBarUp';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+
 
 export default function PaymentsInner() {
+  return ( 
+      <Tab.Navigator initialRouteName="Pending" style={{paddingTop:30}} >
+      <Tab.Screen name="Pending" component={Pending} />
+      <Tab.Screen name="History" component={History} />
+      
+    </Tab.Navigator>
+  );
+}
+
+function Main() {
   return (
-    <View style={styles.container}>
-      <Text>Payments</Text>
+    <View>
+      <Text>Main</Text>
     </View>
-  )
+  );
+}
+
+function Pending() {
+  return (
+    <View style={styles.container} >
+      <Text>Pending</Text>
+    </View>
+  );
+}
+
+function History() {
+  return (
+    <View style={styles.container} >
+      <Text>History</Text>
+    </View>
+  );
 }
 
 
-const styles = StyleSheet.create({
+
+const styles= StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30,
-    flexDirection: 'column',
-    fontFamily: 'sans-serif',
-    backgroundColor: 'none',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    
+    
+    },
+    
+
 })
+     
+
+
+
+
+
+
+ 
