a071d4d0f393fd94d10a8b4979ede36f250909d4
dorr <doratzabi1@gmail.com>
2023-02-10 00:17:19 +0200

    chat_start_implemantion

---
+++ Server/WebAPI/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using Fleck;
+
+
+
+namespace ConsoleApp1
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("Open!");
+                socket.OnClose = () => Console.WriteLine("Close!");
+                socket.OnMessage = message => socket.Send(message);
+            });
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
