fdecaa129d0d14bb291cf970e067b850330c7608
oryan1410 <oryan1410@gmail.com>
2023-02-11 19:28:16 +0200

    after pull

--- Client/Component/AppBarDown.jsx
+++ Client/Component/AppBarDown.jsx
@@ -1,13 +1,8 @@
-import { StyleSheet, View, SafeAreaView, Dimensions, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet } from 'react-native'
+import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Octicons, Ionicons, AntDesign } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-import SettingScreen from './SettingScreen';
-import Contacts from './Contacts';
-import PushNotifications from './PushNotifications';
 
 import Home from '../Component/Home';
 import Payments from '../Component/Payments';
@@ -15,105 +10,61 @@ import Chats from '../Component/Chats';
 import Tasks from '../Component/Tasks';
 import Rights from '../Component/Rights';
 
-const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 export default function AppBarDown() {
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: flexUp }}>
-                <NavigationContainer independent={true} >
-                    <Stack.Navigator
-                        screenOptions={{
-                            headerMode: 'none',
-                        }}
-                    >
-                        <Stack.Screen name='App Bar Up' component={AppBarUp}
-                            
-                        />
-                        <Stack.Screen name='SettingScreen' component={SettingScreen} 
-                        options={{
-                            presentation:'transparentModal',
-                            
-                        }}
-                        />
-                        <Stack.Screen name='PushNotifications' component={PushNotifications} />
-                        <Stack.Screen name='Contacts' component={Contacts} />
-                    </Stack.Navigator>
-                </NavigationContainer>
-            </View>
-
-            <View style={{ flex: flexDown }}>
-                <Tab.Navigator
-                    screenOptions={({ route }) => ({
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconSrc;
-                            if (route.name === 'Home') {
-                                iconSrc = focused ? 'home' : 'home';
-                                return <Octicons name={iconSrc} size={size} color={color} />
-                            } else if (route.name === 'Payments') {
-                                iconSrc = focused ? 'credit-card' : 'credit-card';
-                                return <Octicons name={iconSrc} size={size} color={color} />
-                            } else if (route.name === 'Chats') {
-                                //sliders icon for settings
-                                return <Ionicons name="chatbubble-ellipses-outline" size={size} color={color} />
-                            } else if (route.name === 'Tasks') {
-                                //iconSrc = focused ? 'home' : 'home';
-                                return <Octicons name="checklist" size={size} color={color} />
-                            }
-                            else if (route.name === 'Rights') {
-                                //iconSrc = focused ? 'home' : 'home';
-                                return <Octicons name="question" size={size} color={color} />
-                            }
-                        },
-                        headerShown: false
-                    })
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconSrc;
+                    if (route.name === 'Home') {
+                        iconSrc = focused ? 'home' : 'home';
+                        return <Octicons name={iconSrc} size={size} color={color} />
+                    } else if (route.name === 'Payments') {
+                        iconSrc = focused ? 'credit-card' : 'credit-card';
+                        return <Octicons name={iconSrc} size={size} color={color} />
+                    } else if (route.name === 'Chats') {
+                        //sliders icon for settings
+                        return <Ionicons name="chatbubble-ellipses-outline" size={size} color={color} />
+                    } else if (route.name === 'Tasks') {
+                        //iconSrc = focused ? 'home' : 'home';
+                        return <Octicons name="checklist" size={size} color={color} />
                     }
-                    tabBarOptions={{
-                        activeTintColor: '#548DFF',
-                        inactiveTintColor: '#808080',
-                        style: styles.tabBar,
-                    }}
-                    initialRouteName="Home"
-                >
-                    <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'Home' }} />
-                    <Tab.Screen name="Payments" component={Payments} options={{ tabBarLabel: 'Payments' }} />
-                    <Tab.Screen name="Chats" component={Chats} options={{ tabBarLabel: 'Chats' }} />
-                    <Tab.Screen name="Tasks" component={Tasks} options={{ tabBarLabel: 'Tasks' }} />
-                    <Tab.Screen name="Rights" component={Rights} options={{ tabBarLabel: 'Rights' }} />
-                </Tab.Navigator>
-            </View>
-        </View>
+                    else if (route.name === 'Rights') {
+                        //iconSrc = focused ? 'home' : 'home';
+                        return <Octicons name="question" size={size} color={color} />
+                    }
+                },
+                headerShown: false
+            })
+            }
+            tabBarOptions={{
+                activeTintColor: '#548DFF',
+                inactiveTintColor: '#808080',
+                style: styles.tabBar,
+            }}
+            initialRouteName="Home"
+        >
+            <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'Home' }} />
+            {/*בעת ניווט למסך הבית - תבוצע פעולת גט שתמשוך את הפרטים- לוח שנה למשתמש- סוג לוח שנה,
+                משימה אישית/משימה למטופל- מספר משימה, שם משימה, תאריך התחלה, תאריך סוף, הערות, סטטוס */}
+            <Tab.Screen name="Payments" component={Payments} options={{ tabBarLabel: 'Payments', unmountOnBlur: true }} />
+            {/*בעת ניווט למסך תשלומים - למסך הראשי אין צורך בביצוע פעולות, לאחר בחירת המסך הרצוי(תשלומים/משכורות) יבוצעו פעולות גט)
+                מסך תשלומים- שני תתי מסכים- פירוט יבוצע בקומפוננטת הניווט במסכים היעודיים
+                */}
+            <Tab.Screen name="Chats" component={Chats} options={{ tabBarLabel: 'Chats' }} />
+            {/*בהתאים למימוש הצ'אט ושמירת הסטוריית השיחות - תתבצע פעולת גט אשר תשלוף את היסטוריית השיחות. 
+            לכל שיחה ישלף שם המשתמש השני/הקבוצה, סטטוס השיחה (האם יש הודעה חדשה אשר לא נקראה) ותוצג ההודעה האחרונה אשר נשלחה.*/}
+            <Tab.Screen name="Tasks" component={Tasks} options={{ tabBarLabel: 'Tasks', unmountOnBlur: true  }} />
+            {/*בעת ניווט למסך תשלומים - תבוצע פעולת גט שתמשוך את הפרטים- ,
+                 משימה אישית/משימה למטופל- מספר משימה, שם משימה, תאריך התחלה, תאריך סוף, הערות, סטטוס(לפי מספר משתמש),
+                  שדות הייחודיים לטבלת משימה למטופל אשר יימשכו- מספר מטופל,מספר רשימה*/}
+            <Tab.Screen name="Rights" component={Rights} options={{ tabBarLabel: 'Rights' }} />
+            {/**לא נדרשת פעולה הקשורה במסדר הנתונים על מנת לטעון את המסך הנדרש */}
+        </Tab.Navigator>
     )
 }
 
-function AppBarUp({ navigation }) {
-    return (
-        <SafeAreaView style={styles.Header}>
-            {/* Left - Settings - ClickAble - To SettingScreen */}
-            <View style={styles.ProfileIcon}>
-                <TouchableOpacity onPress={() => { navigation.navigate('SettingScreen') }}>
-                    <Image source={require('../images/icons/Profile.png')} style={{ width: 24, height: 24 }} />
-                </TouchableOpacity>
-            </View>
-            {/* Middle - Logo - not clickable */}
-            <View style={styles.logo}>
-                <Image source={require('../images/logo_New_Small.png')} style={{ width: 50, height: 50 }} />
-            </View>
-            {/* Right - Notifications - ClickAble - To PushNotifications */}
-            <View style={styles.Notifications}>
-                <TouchableOpacity onPress={() => { navigation.navigate('PushNotifications') }}>
-                    <Image source={require('../images/icons/notifications.png')} style={{ width: 24, height: 24 }} />
-                </TouchableOpacity>
-            </View>
-            {/* Right - Contacts - ClickAble - To Contacts */}
-            <View style={styles.ContactICON}>
-                <TouchableOpacity onPress={() => { navigation.navigate('Contacts') }}>
-                    <AntDesign name="contacts" size={24} color={'#000000'} />
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    )
-}
 
 const styles = StyleSheet.create({
     container: {
@@ -121,13 +72,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+
     },
+
     tabBar: {
         backgroundColor: '#fff',
+        paddingTop: 10,
     },
-    ProfileIcon: { marginLeft: Dimensions.get('screen').width * 0.075, flex: 1, justifyContent: 'space-between', alignContent: 'space-between' },
-    logo: { flex: 1, marginLeft: Dimensions.get('screen').width * 0.01 },
-    Notifications: { marginRight: Dimensions.get('screen').width * 0.01 },
-    ContactICON: { marginRight: Dimensions.get('screen').width * 0.05 },
-    Header: { flexDirection: 'row', backgroundColor: 'white', margin: 0, padding: 20, height: 100, alignItems: 'center', zIndex: 0 },
 });
--- Client/Component/AppBarUp.jsx
+++ Client/Component/AppBarUp.jsx
@@ -1,27 +1,76 @@
-import { View, TouchableOpacity, SafeAreaView, Image, Dimensions } from 'react-native'
+import { View, TouchableOpacity, SafeAreaView, Image, Dimensions,Button } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import AppBarDown from './AppBarDown';
 import SettingScreen from './SettingScreen';
 import Contacts from './Contacts';
 import PushNotifications from './PushNotifications';
 
 const Stack = createStackNavigator();
-export default function CustomHeader(props) {
+export default function CustomHeader(props, {navigation}) {
     return (
-        <View>
-            <NavigationContainer independent={true} >
-                <Stack.Navigator headerMode='none' >
-                    <Stack.Screen name='AppBarUp' component={AppBarUp} />
-                    <Stack.Screen name='SettingScreen' component={SettingScreen} />
-                    <Stack.Screen name='PushNotifications' component={PushNotifications} />
-                    <Stack.Screen name='Contacts' component={Contacts} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </View>
+
+
+        <NavigationContainer independent={true} zIndex='0'   >
+            <Stack.Navigator>
+                <Stack.Screen name='AppBarUp' component={AppBarUp} options={(route) => ({
+                    headerShown: false,  
+                                   
+                    style: {
+                        flex: 1,
+                    },
+
+                })} />
+                <Stack.Screen name='SettingScreen' component={SettingScreen} options={() => ({
+                    headerShown: true,
+                    headerTitle: 'check',
+                    presentation: 'stack',
+                    cardOverlayEnabled: true,
+                  
+                })} />
+                <Stack.Screen name='PushNotifications' component={PushNotifications} options={() => ({
+                    headerShown: true,
+                    headerTitle: 'bla',
+                    presentation: 'stack',
+                    cardOverlayEnabled: true,
+                })} />
+                <Stack.Screen name='Contacts' component={Contacts} />
+            </Stack.Navigator>
+
+        </NavigationContainer>
+        
+        // <View style={{flex:1}}>
+        //     <NavigationContainer independent={true} >
+        //         <Stack.Navigator headerMode='none' >
+        //             <Stack.Screen name='AppBarUp' component={AppBarUp} />
+        //             <Stack.Screen name='SettingScreen' component={SettingScreen} />
+        //             <Stack.Screen name='PushNotifications' component={PushNotifications} />
+        //             <Stack.Screen name='Contacts' component={Contacts} />
+        //         </Stack.Navigator>
+        //     </NavigationContainer>
+        // </View>
     );
+    {/* <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerTitle: (props) => <LogoTitle {...props} />,
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                color="#00cc00"
+              />
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer> */}
+
 }
 
 function AppBarUp({ navigation }) {
@@ -33,6 +82,8 @@ function AppBarUp({ navigation }) {
                     <Image source={require('../images/icons/Profile.png')} style={{ width: 24, height: 24 }} />
                 </TouchableOpacity>
             </View>
+            {/*במעבר למסך ההגדרות ישלפו הגדרות המתשתמש הנבחרות לתפעול האפליקתיה.
+            בנוסף תתבצע פעות גט למסד הנתונים וישלפו כלל הפרטים של המשתמש, על מנת לאפשר שינוי של הפרופיל. */}
             {/* Middle - Logo - not clickable */}
             <View style={{ flex: 1, marginLeft: Dimensions.get('screen').width * 0.01 }}>
                 <Image source={require('../images/logo_New_Small.png')} style={{ width: 50, height: 50 }} />
@@ -49,6 +100,9 @@ function AppBarUp({ navigation }) {
                     <AntDesign name="contacts" size={24} color={'#000000'} />
                 </TouchableOpacity>
             </View>
+            {/*בעת ניווט למסך אנשי הקשר תתבצע פעולת גט (באמצעות מספר מטופל- המקושר למשתמש), וישלפו נתוני איש הקשר 
+            הנתונים הנשלפים הינם שם, מספר טלפון, מספר נייד, אימייל, תפקיד, הערות ומספר מטופל.
+            בנוסף ישלפו מספרים חשובים אשר נשמרו על ידי המשתמש. */}
         </SafeAreaView>
     )
 }
