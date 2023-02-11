c717398b834066b9f04adf8cebcfb9e5338b9c91
Noam Norman <normannoam21@gmail.com>
2023-02-11 16:39:22 +0200

    NEW Model UPDATE

---
+++ Server/NEW/DATA/WebApi/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using DATA;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        igroup194_Model db = new igroup194_Model();
+        // GET api/<controller>
+        [Route("GetUser/{id}")]
+        public IHttpActionResult GetUser(int id)
+        {
+            try
+            {
+                var user = db.tblUser.Where(x => x.Id == id).FirstOrDefault();
+                return Ok("Full Name: " + user.FirstName + " " + user.LastName +", Email: "+ user.Email);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
