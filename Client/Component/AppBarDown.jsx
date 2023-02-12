1e536e7c93914cf7d4596b414c38b680efd0d29e
oryan1410 <oryan1410@gmail.com>
2023-02-12 20:22:19 +0200

    .

--- Client/Component/AppBarDown.jsx
+++ Client/Component/AppBarDown.jsx
@@ -1,82 +1,82 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Octicons, Ionicons, AntDesign } from '@expo/vector-icons';
+// import { StyleSheet } from 'react-native'
+// import React from 'react'
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { Octicons, Ionicons, AntDesign } from '@expo/vector-icons';
 
 
-import Home from '../Component/Home';
-import Payments from '../Component/Payments';
-import Chats from '../Component/Chats';
-import Tasks from '../Component/Tasks';
-import Rights from '../Component/Rights';
+// import Home from '../Component/Home';
+// import Payments from '../Component/Payments';
+// import Chats from '../Component/Chats';
+// import Tasks from '../Component/Tasks';
+// import Rights from '../Component/Rights';
 
-const Tab = createBottomTabNavigator();
-export default function AppBarDown() {
-    return (
-        <Tab.Navigator
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                    let iconSrc;
-                    if (route.name === 'Home') {
-                        iconSrc = focused ? 'home' : 'home';
-                        return <Octicons name={iconSrc} size={size} color={color} />
-                    } else if (route.name === 'Payments') {
-                        iconSrc = focused ? 'credit-card' : 'credit-card';
-                        return <Octicons name={iconSrc} size={size} color={color} />
-                    } else if (route.name === 'Chats') {
-                        //sliders icon for settings
-                        return <Ionicons name="chatbubble-ellipses-outline" size={size} color={color} />
-                    } else if (route.name === 'Tasks') {
-                        //iconSrc = focused ? 'home' : 'home';
-                        return <Octicons name="checklist" size={size} color={color} />
-                    }
-                    else if (route.name === 'Rights') {
-                        //iconSrc = focused ? 'home' : 'home';
-                        return <Octicons name="question" size={size} color={color} />
-                    }
-                },
-                headerShown: false
-            })
-            }
-            tabBarOptions={{
-                activeTintColor: '#548DFF',
-                inactiveTintColor: '#808080',
-                style: styles.tabBar,
-            }}
-            initialRouteName="Home"
-        >
-            <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'Home' }} />
-            {/*בעת ניווט למסך הבית - תבוצע פעולת גט שתמשוך את הפרטים- לוח שנה למשתמש- סוג לוח שנה,
-                משימה אישית/משימה למטופל- מספר משימה, שם משימה, תאריך התחלה, תאריך סוף, הערות, סטטוס */}
-            <Tab.Screen name="Payments" component={Payments} options={{ tabBarLabel: 'Payments', unmountOnBlur: true }} />
-            {/*בעת ניווט למסך תשלומים - למסך הראשי אין צורך בביצוע פעולות, לאחר בחירת המסך הרצוי(תשלומים/משכורות) יבוצעו פעולות גט)
-                מסך תשלומים- שני תתי מסכים- פירוט יבוצע בקומפוננטת הניווט במסכים היעודיים
-                */}
-            <Tab.Screen name="Chats" component={Chats} options={{ tabBarLabel: 'Chats' }} />
-            {/*בהתאים למימוש הצ'אט ושמירת הסטוריית השיחות - תתבצע פעולת גט אשר תשלוף את היסטוריית השיחות. 
-            לכל שיחה ישלף שם המשתמש השני/הקבוצה, סטטוס השיחה (האם יש הודעה חדשה אשר לא נקראה) ותוצג ההודעה האחרונה אשר נשלחה.*/}
-            <Tab.Screen name="Tasks" component={Tasks} options={{ tabBarLabel: 'Tasks', unmountOnBlur: true  }} />
-            {/*בעת ניווט למסך תשלומים - תבוצע פעולת גט שתמשוך את הפרטים- ,
-                 משימה אישית/משימה למטופל- מספר משימה, שם משימה, תאריך התחלה, תאריך סוף, הערות, סטטוס(לפי מספר משתמש),
-                  שדות הייחודיים לטבלת משימה למטופל אשר יימשכו- מספר מטופל,מספר רשימה*/}
-            <Tab.Screen name="Rights" component={Rights} options={{ tabBarLabel: 'Rights' }} />
-            {/**לא נדרשת פעולה הקשורה במסדר הנתונים על מנת לטעון את המסך הנדרש */}
-        </Tab.Navigator>
-    )
-}
+// const Tab = createBottomTabNavigator();
+// export default function AppBarDown() {
+//     return (
+//         <Tab.Navigator
+//             screenOptions={({ route }) => ({
+//                 tabBarIcon: ({ focused, color, size }) => {
+//                     let iconSrc;
+//                     if (route.name === 'Home') {
+//                         iconSrc = focused ? 'home' : 'home';
+//                         return <Octicons name={iconSrc} size={size} color={color} />
+//                     } else if (route.name === 'Payments') {
+//                         iconSrc = focused ? 'credit-card' : 'credit-card';
+//                         return <Octicons name={iconSrc} size={size} color={color} />
+//                     } else if (route.name === 'Chats') {
+//                         //sliders icon for settings
+//                         return <Ionicons name="chatbubble-ellipses-outline" size={size} color={color} />
+//                     } else if (route.name === 'Tasks') {
+//                         //iconSrc = focused ? 'home' : 'home';
+//                         return <Octicons name="checklist" size={size} color={color} />
+//                     }
+//                     else if (route.name === 'Rights') {
+//                         //iconSrc = focused ? 'home' : 'home';
+//                         return <Octicons name="question" size={size} color={color} />
+//                     }
+//                 },
+//                 headerShown: false
+//             })
+//             }
+//             tabBarOptions={{
+//                 activeTintColor: '#548DFF',
+//                 inactiveTintColor: '#808080',
+//                 style: styles.tabBar,
+//             }}
+//             initialRouteName="Home"
+//         >
+//             <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'Home' }} />
+//             {/*בעת ניווט למסך הבית - תבוצע פעולת גט שתמשוך את הפרטים- לוח שנה למשתמש- סוג לוח שנה,
+//                 משימה אישית/משימה למטופל- מספר משימה, שם משימה, תאריך התחלה, תאריך סוף, הערות, סטטוס */}
+//             <Tab.Screen name="Payments" component={Payments} options={{ tabBarLabel: 'Payments', unmountOnBlur: true }} />
+//             {/*בעת ניווט למסך תשלומים - למסך הראשי אין צורך בביצוע פעולות, לאחר בחירת המסך הרצוי(תשלומים/משכורות) יבוצעו פעולות גט)
+//                 מסך תשלומים- שני תתי מסכים- פירוט יבוצע בקומפוננטת הניווט במסכים היעודיים
+//                 */}
+//             <Tab.Screen name="Chats" component={Chats} options={{ tabBarLabel: 'Chats' }} />
+//             {/*בהתאים למימוש הצ'אט ושמירת הסטוריית השיחות - תתבצע פעולת גט אשר תשלוף את היסטוריית השיחות. 
+//             לכל שיחה ישלף שם המשתמש השני/הקבוצה, סטטוס השיחה (האם יש הודעה חדשה אשר לא נקראה) ותוצג ההודעה האחרונה אשר נשלחה.*/}
+//             <Tab.Screen name="Tasks" component={Tasks} options={{ tabBarLabel: 'Tasks', unmountOnBlur: true  }} />
+//             {/*בעת ניווט למסך תשלומים - תבוצע פעולת גט שתמשוך את הפרטים- ,
+//                  משימה אישית/משימה למטופל- מספר משימה, שם משימה, תאריך התחלה, תאריך סוף, הערות, סטטוס(לפי מספר משתמש),
+//                   שדות הייחודיים לטבלת משימה למטופל אשר יימשכו- מספר מטופל,מספר רשימה*/}
+//             <Tab.Screen name="Rights" component={Rights} options={{ tabBarLabel: 'Rights' }} />
+//             {/**לא נדרשת פעולה הקשורה במסדר הנתונים על מנת לטעון את המסך הנדרש */}
+//         </Tab.Navigator>
+//     )
+// }
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
 
-    },
+//     },
 
-    tabBar: {
-        backgroundColor: '#fff',
-        paddingTop: 10,
-    },
-});
+//     tabBar: {
+//         backgroundColor: '#fff',
+//         paddingTop: 10,
+//     },
+// });
--- Client/Component/Tasks.jsx
+++ Client/Component/Tasks.jsx
@@ -1,40 +1,34 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
-import CustomHeader from './AppBarUp';
-
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 const Tab = createMaterialTopTabNavigator();
 
-
-
 export default function Tasks() {
   return (
-    <Tab.Navigator initialRouteName="Main" tabBarPosition='top' tabBarOptions={{
-      pressColor: 'blue',
-      pressOpacity: 0.5,
-      labelStyle: {
-        fontSize: 17,
-        color: 'grey',
-        textTransform: 'none',       
-        alignItems: 'center',
-      },
-      style: {
-        
-        backgroundColor: 'transparent',
-        height: 50,
-        
-      
-        
-       
-        
-      },
-      indicatorStyle: {
-        backgroundColor: 'blue',
-        height: 4,
-      },
-      tabStyle: {width: 'auto', marginLeft: 10, alignItems: 'center'},
-    }} >
+    <Tab.Navigator
+      initialRouteName="Main"
+      tabBarOptions={{
+        pressColor: '#548DFF',
+        pressOpacity: 0.5,
+        labelStyle: {
+          fontSize: 16, // <-- change this size to 18 when we have the font family 'Urbanist'
+          fontWeight: 'bold',
+          color: '#9E9E9E',
+          textTransform: 'none',
+          alignItems: 'center',
+        },
+        style: {
+          backgroundColor: 'transparent',
+          marginTop: 25,
+          height: 50,
+        },
+        indicatorStyle: {
+          backgroundColor: '#548DFF',
+          height: 4,
+        },
+        tabStyle: { width: 'auto', marginLeft: 10, alignItems: 'center' },
+      }} >
       <Tab.Screen style={styles.container2} name="Main" component={Main} />
       <Tab.Screen name="General" component={General} />
       <Tab.Screen name="Shop" component={Shop} />
@@ -75,30 +69,11 @@ function Medicine() {
   );
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
-    flexDirection: 'column',    
-    fontFamily:'sans-serif',
+    flexDirection: 'column',
+    fontFamily: 'sans-serif',
     backgroundColor: 'none',
-    
-    
-    },
-    container2: {
-      flex: 1,
-      
-      flexDirection: 'column',
-      fontFamily:'sans-serif',
-      backgroundColor: 'none',
-fontWeight: 'bold',      }
-
+  }
 })
-     
-
-
-
-
-
-
- 
