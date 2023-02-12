40227ac86cd115f98fbc7740cfe3f550e9d47350
oryan1410 <oryan1410@gmail.com>
2023-02-12 23:24:44 +0200

    font in settings screen

--- Client/Component/SettingScreen.jsx
+++ Client/Component/SettingScreen.jsx
@@ -1,10 +1,30 @@
 import { TouchableOpacity, View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import {useFonts } from 'expo-font'
+import { useCallback } from 'react';
+import * as Font from 'expo-font';
 
-export default function SettingScreen() {
+
+export default function SettingScreen({navigation}) {
+
+    const [fontsLoaded] = useFonts({
+        'Urbanist-Black': require('../assets/fonts/Urbanist-Black.ttf'),
+      });
+    
+      const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded]);
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+    
+      
     return (
         <SafeAreaView style={{flex:1}}>
-            <Text>
+            <Text style={{fontFamily:'Urbanist-Black'}}>
                 SettingScreen
             </Text>
         </SafeAreaView >
