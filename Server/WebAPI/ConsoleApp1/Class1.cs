b046f0b63f31e5d067bc5cff1efeafccaf0c4a87
dorr <doratzabi1@gmail.com>
2023-02-13 20:20:06 +0200

    Chat server side

---
+++ Server/WebAPI/ConsoleApp1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fleck;
+
+namespace ConsoleApp1
+{
+    public class ClientData
+    {
+        public IWebSocketConnection Connection { get; set; }
+        public string Username { get; set; }
+        public bool Shouldtranslate { get; set; }
+
+        public string ConnectionId { get; set; }
+
+
+    }
+
+    public class MessageData
+    {
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+
+        public string Username { get; set; }
+        public string Usertarget { get; set; }
+        public string Content { get; set; }
+        public bool ShouldTranslate { get; set; }
+
+        public string MessageType { get; set; }
+    }
+
+}
--- Server/WebAPI/ConsoleApp1/Program.cs
+++ Server/WebAPI/ConsoleApp1/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using Fleck;
+using Google.Cloud.Translation.V2;
+using Newtonsoft.Json;
 
 
 
@@ -13,14 +15,71 @@ namespace ConsoleApp1
    
     internal class Program
     {
+        private static Dictionary<string,ClientData> clients= new Dictionary<string,ClientData>();
         static void Main(string[] args)
         {
+            string ApiGooglekey ="AIzaSyDA-GbcFjJNNLqEFteW3ic3l9s7zhpbKwA";
+            TranslationClient client = TranslationClient.CreateFromApiKey(ApiGooglekey);
             var server = new WebSocketServer("ws://0.0.0.0:8181");
             server.Start(socket =>
             {
-                socket.OnOpen = () => Console.WriteLine("Open!");
-                socket.OnClose = () => Console.WriteLine("Close!");
-                socket.OnMessage = message => socket.Send(message);
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                };
+          
+                    socket.OnClose = () => { Console.WriteLine("Close!"); };
+                    socket.OnMessage = (message) =>
+                    {
+
+                        MessageData messagedata = JsonConvert.DeserializeObject<MessageData>(message);
+                        switch (messagedata.MessageType)
+                        {
+                            case "message":
+
+                                var recipient = clients.Values.FirstOrDefault(c => c.Username == messagedata.Usertarget);
+                                if (recipient != null)
+                                {
+                                    if (recipient.Shouldtranslate)
+                                    {
+                                        TranslationResult result = client.TranslateText(messagedata.Content, "he", "en");
+                                        Console.WriteLine(result.TranslatedText);
+                                        string translatedText = result.TranslatedText;
+                                        recipient.Connection.Send(translatedText);
+
+                                    }
+                                    else
+                                    {
+                                        recipient.Connection.Send(messagedata.Content);
+                                    }
+                                }
+                                break;
+
+                            case "translate preference":
+                                if (clients.ContainsKey(messagedata.Sender))
+                                {
+                                    var client2 = clients[messagedata.Sender];
+                                    client2.Shouldtranslate = messagedata.ShouldTranslate;
+
+                                }
+                                break;
+
+                            case "clientData":
+
+                                ClientData clientdata = new ClientData { ConnectionId = socket.ConnectionInfo.Id.ToString() };
+                                clientdata.Connection = socket;
+                                clientdata.Username = messagedata.Username;
+                                clientdata.Shouldtranslate = true;
+                                clients.Add(clientdata.ConnectionId, clientdata);
+                                break;
+
+                            default:
+                                break;
+                        }
+
+
+                    };
+                
             });
 
             var input = Console.ReadLine();
