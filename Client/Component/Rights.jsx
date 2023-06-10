fdfe845cc2335d5cb04a110f9efcb2c3e1227b2d
Amitaifink <amitai38@hotmail.com>
2023-06-10 12:18:11 +0300

    concect the api for davinci-003

--- Client/Component/Rights.jsx
+++ Client/Component/Rights.jsx
@@ -1,33 +1,23 @@
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Dimensions, TextInput, ScrollView } from 'react-native';
-import { KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import React, { useEffect, useState, useCallback } from 'react';
-import { AddBtn, NewTaskModal } from './HelpComponents/AddNewTask'
-// import { TextInput } from 'react-native-paper';
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
-import { useUserContext } from '../UserContext';
-import { useIsFocused } from '@react-navigation/native';
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Dimensions, TextInput, ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard ,ActivityIndicator} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { Overlay } from '@rneui/themed';
+import { useUserContext } from '../UserContext';
+import { useIsFocused } from '@react-navigation/native';
+import { AddBtn, NewTaskModal } from './HelpComponents/AddNewTask';
+import { OpenAiKey } from '@env'
 
-// This is the Rights page - the page that the user can ask qustions about his rights and get answers from the admin
+const apiKeyGpt = OpenAiKey;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+
 export default function Rights() {
   const { userContext } = useUserContext();
   const isFocused = useIsFocused();
   const [modalVisible, setModalVisible] = useState(false);
   const [visible, setVisible] = useState(false);
-
-  const toggleOverlay = () => {
-    setVisible(!visible);
-  };
-
-  const handleAddBtnPress = () => {
-    setModalVisible(true);
-  };
-  const handleModalClose = () => {
-    setModalVisible(false);
-  };
   const [value, setValue] = useState(null);
   const [open, setOpen] = useState(false);
   const [items, setItems] = useState([
@@ -37,21 +27,73 @@ export default function Rights() {
     { label: 'Health', value: 'Health' },
     { label: 'Other', value: 'Other' },
   ]);
+  const [question, setQuestion] = useState('');
+  const [gpt3Answer, setGpt3Answer] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+    if (visible === false) {
+      setQuestion('');
+      setValue(null);
+    }
+  };
+
+  // Function to send the question to GPT-3 API and get an answer
+  const startGptAnswer = async () => {
+    if (question === '' || value === null) {
+      return;
+    }
+    setIsLoading(true);
+    toggleOverlay();
+    // Adjust the prompt to include the selected category and the question
+    const prompt = `Category: ${value}\nQuestion: ${question}\n\nContext: In Israel, foreign workers in the field of caregiver for the elderly have specific rights and regulations. It is important to provide accurate and reliable information. Please provide an answer that is specific to Israel's laws and guidelines.\n\nAnswer:`;
+    // Make the API call and handle the response
+    const apiKey = apiKeyGpt;
+    const apiResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        'prompt': prompt,
+        'max_tokens': 400, // Set this to the number of tokens you want the completion to be
+        'temperature': 0.8,
+        'top_p': 1,
+      }),
+    });
+    //Process the API response and extract the answer
+    const data = await apiResponse.json();
+    const answer = data.choices[0].text.trim();
+    console.log(answer);
+    // Set the answer in the state
+    setGpt3Answer(answer);
+    setIsLoading(false);
+    // Show the answer overlay
+
+  };
+
+  const handleAddBtnPress = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
 
   const onOpenDropdown = useCallback(() => {
     setOpen(true);
   }, []);
 
-
   return (
     <SafeAreaView style={styles.container}>
       <LinearGradient
-        // Background Linear Gradient
         colors={['#E6EBF2', '#548DFF']}
         style={styles.background}
       />
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={{ flex: 1 }}>
             <View style={{ flex: 1 }}>
               <Text style={styles.headerTxt}>Rights</Text>
@@ -59,13 +101,12 @@ export default function Rights() {
 
             <View style={{ flex: 1.5 }}>
               <View>
-
                 <Text style={styles.smHeader}>Categories</Text>
                 <DropDownPicker
-                  placeholder={'Select a category'}
+                  placeholder="Select a category"
                   open={open}
                   onOpen={onOpenDropdown}
-                  listMode='SCROLLVIEW'
+                  listMode="SCROLLVIEW"
                   value={value}
                   items={items}
                   setOpen={setOpen}
@@ -74,11 +115,8 @@ export default function Rights() {
                   containerStyle={{ height: 40, width: SCREEN_WIDTH * 0.85, marginVertical: 10 }}
                   placeholderStyle={{ fontFamily: 'Urbanist-Light', fontSize: 16, color: '#000' }}
                   style={{ backgroundColor: '#fff', borderRadius: 16, borderWidth: 1.5, borderColor: '#E6EBF2', height: 54 }}
-                  itemStyle={{
-                    justifyContent: 'flex-start'
-                  }}
+                  itemStyle={{ justifyContent: 'flex-start' }}
                   dropDownContainerStyle={{ backgroundColor: '#fff', borderRadius: 16, borderWidth: 1.5, borderColor: '#E6EBF2' }}
-                  // dropDownStyle={{ backgroundColor: '#fafafa' }}
                   onChangeItem={item => console.log(item.label, item.value)}
                 />
 
@@ -86,34 +124,40 @@ export default function Rights() {
                 <TextInput
                   style={styles.inputTxt}
                   placeholder="What do you want to ask?"
-                  // multiline
                   numberOfLines={4}
-                  // maxLength={300}
-                  placeholderTextColor={'#000'}
-                  returnKeyType='done' // Customize the return key type
+                  placeholderTextColor="#000"
+                  returnKeyType="done"
+                  onChangeText={text => setQuestion(text)}
+                  value={question}
                 />
-
               </View>
-              <View style={[styles.btnAskContainer, open == true ? { display: 'none' } : { display: 'flex' }]}>
-                <TouchableOpacity style={styles.btnAsk} onPress={
-                  () => {
-                    console.log('pressed');
-                    toggleOverlay();
-                  }
-                }>
+
+              <View style={[styles.btnAskContainer, open === true ? { display: 'none' } : { display: 'flex' }]}>
+                <TouchableOpacity style={styles.btnAsk} onPress={startGptAnswer}>
                   <Text style={{ fontFamily: 'Urbanist-Bold', fontSize: 16, color: '#fff' }}>Ask Now</Text>
                 </TouchableOpacity>
               </View>
             </View>
+
             <Overlay
               isVisible={visible}
               onBackdropPress={toggleOverlay}
               overlayStyle={{ width: SCREEN_WIDTH * 0.85, height: SCREEN_HEIGHT * 0.5, borderRadius: 16, padding: 20 }}
             >
-              <ScrollView alwaysBounceVertical={false}>
+              <View>
                 <Text style={[styles.smHeader, { textAlign: 'center', fontSize: 22 }]}>Answer</Text>
-                <Text style={{ fontFamily: 'Urbanist-Light', fontSize: 16, marginTop: 20 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam sit amet aliquet tincidunt, nunc nisl ultrices nunc, quis aliquam nunc nisl nec nisl. Sed euismod, diam sit amet aliquet tincidunt, nunc nisl ultrices nunc, quis aliquam nunc nisl nec nisl.</Text>
-              </ScrollView>
+              </View>
+              <View>
+
+                <ScrollView alwaysBounceVertical={false} style={styles.answerScrollView}>
+                  {isLoading ? (
+                    <ActivityIndicator size="large" color="red" />
+                  ) : (
+                    <Text style={{ fontFamily: 'Urbanist-Light', fontSize: 16, marginTop: 15 }}>{gpt3Answer}</Text>
+                  )}
+                </ScrollView>
+              </View>
+
               <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
                 <TouchableOpacity style={[styles.btnAsk, { width: '100%' }]} onPress={toggleOverlay}>
                   <Text style={{ fontFamily: 'Urbanist-Bold', fontSize: 16, color: '#fff' }}>Close</Text>
@@ -123,11 +167,13 @@ export default function Rights() {
           </View>
         </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
+
       <View style={styles.addBtnView}>
         <AddBtn onPress={handleAddBtnPress} />
       </View>
+
       <NewTaskModal isVisible={modalVisible} onClose={handleModalClose} />
-    </SafeAreaView >
+    </SafeAreaView>
   );
 }
 
@@ -141,6 +187,13 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 20,
     right: 10,
+
+  },
+  answerScrollView: {
+    maxHeight: SCREEN_HEIGHT * 0.3,  // Adjust this value based on your requirements.
+    width: '100%',
+    marginTop: 10,
+    marginBottom: 10,
   },
   inputTxt: {
     fontFamily: 'Urbanist-Light',
@@ -155,7 +208,6 @@ const styles = StyleSheet.create({
     borderWidth: 1.5,
     borderColor: '#E6EBF2',
     padding: 10,
-    // textAlignVertical: 'top',
   },
   smHeader: {
     fontSize: 18,
@@ -192,5 +244,5 @@ const styles = StyleSheet.create({
   btnAskContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
